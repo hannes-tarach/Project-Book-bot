8d24e592adfbc690651ff9927a3885a248d28ca0
hannes.tarach@gmail.com <hannes.tarach@gmail.com>
2024-03-30 18:09:02 +0100

    Finished the project

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path) 
     word_counter = count_words(text)
-    character_counter = count_char(text)
+    dictionary = count_char(text)
+    character_counter = sort_character_frequency(dictionary)  # Ändra?
     print(f"The book contains {word_counter} number of words!")
-    print(f"The book contains {character_counter} number of words!")
+    for char, count in character_counter:
+        print(f"The character '{char}' was found {count} times")
 
 
 def get_book_text(path):
@@ -16,14 +18,34 @@ def get_book_text(path):
 
 # This function takes the text from the book as a string, and returns the number of words in the string.
 def count_words(text):
-    words = text.split() 
+    words = text.split()
     return len(words)
 
 
 # Function takes text from the book as a string, and returns the number of times each character appears in the string
 def count_char(text):
     characters = text.lower()
-    return len(characters)
+    character_frequency = {}
+    
+    for char in characters:  # Ensure you're iterating over 'characters', the lowercase version of the text
+        if char.isalpha():   
+            if char in character_frequency:
+                character_frequency[char] += 1
+            else:
+                character_frequency[char] = 1
+    return character_frequency    
+
+
+def sort_character_frequency(dictionary):
+    items = dictionary.items()  # List-like object of tuples (character, count)
+    sorted_items = sorted(items, key=lambda x: x[1], reverse=True)  # Sorts the list of tuples by count in descending order
+
+    return sorted_items
+
+
+
+    
+
 
 if __name__ == "__main__":
     main()
